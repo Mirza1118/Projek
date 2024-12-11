1a80320d5655d92dc0ca37879f781fe7cc4b2257
Mirza1118 <micheljordan121@gmail.com>
2024-12-11 09:29:53 +0700

    Add files via upload

---
+++ Web/Web/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- My Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- My Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            <img id="logo" src="../Web/img/logo.png">
+            <h3>RPL <br>SMKN 10 Surabaya</h3>
+        </div>
+        <div class="navExtra">
+            <a href=""><i class="fas fa-home"></i>
+                <p>Home</p>
+            </a>
+            <a href=""><i class="fas fa-users"></i>
+                <p>Tentang</p>
+            </a>
+            <a href=""><i class="fas fa-user-circle"></i>
+                <p>Profil</p>
+            </a>
+            <a href=""><i class="fas fa-phone-alt"></i>
+                <p>Kontak</p>
+            </a>
+        </div>
+    </nav>
+    <div class="page1">
+        <img id="background" src="./img/images.jpg">
+    </div>
+    <div class="page2">
+        <div class="paragraf">
+            <h2>SELAMAT DATANG DI <br>RPL SMKN 10 SURABAYA</h2>
+        </div>
+        <div class="content">
+            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ea distinctio saepe ad libero, quo nostrum perferendis sit? Perferendis pariatur nobis assumenda officia iusto quis soluta ex aperiam aspernatur incidunt hic iure earum sit esse consequatur magnam, accusamus ratione non fugit, illum harum unde tenetur voluptas. Neque est at, ducimus reprehenderit doloribus distinctio ut cumque incidunt quae adipisci atque repellendus harum odit, sit cum magnam magni commodi minus voluptates quia ratione ipsam. Facere voluptate ea fugit excepturi error aliquid illo, quod voluptates deleniti! Voluptatum, ex. Rem ipsa at delectus suscipit modi! Eius, exercitationem voluptates architecto sunt nemo expedita ipsum fugiat.</P>
+            <img src="./img/WhatsApp Image 2024-02-29 at 09.05.40_fdf68d8c.jpg" width="15%">
+        </div>
+    </div>
+    <div class="page3">
+        <a href=""></a>
+        <div class="content">
+            <img src="./img/WhatsApp Image 2024-02-29 at 09.05.40_fdf68d8c.jpg" width="30%" height="30%">
+            <div class="text">
+                <h2>Beberapa prospek kerja yang dapat <br>dikejar oleh lulusan RPL antara lain:</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum omnis iure corrupti quas nostrum, quibusdam voluptas aliquid officiis praesentium eveniet explicabo placeat totam, voluptatum officia alias labore vero soluta fugiat eligendi, quidem nemo deleniti error veritatis? Recusandae repellat quas exercitationem, minima commodi consequatur debitis ducimus, eveniet quidem, rerum ut eligendi accusantium. Repellendus, omnis ut? Eius, quos molestias! Neque quidem reiciendis non delectus. Quia perferendis placeat nam, nihil expedita at iste modi labore aliquid cumque ea, iure eaque alias atque ipsam consequuntur dolorem. Distinctio, veniam aperiam velit, fugit numquam vel est nihil dolores ut quasi sunt dolore placeat, minus maxime quaerat explicabo sed ducimus reiciendis dolor eligendi? Modi reprehenderit eaque a quibusdam ea harum soluta quas quidem in magni sint ullam necessitatibus voluptatibus quo esse obcaecati nihil facilis mollitia, doloremque tempora est reiciendis. Veniam enim minima aliquam aut reprehenderit deserunt iusto assumenda dolores quae, velit laborum quod incidunt modi labore reiciendis animi ipsum quibusdam rem exercitationem ea! Quibusdam aperiam aliquam eos voluptates facere, iure consectetur quas tempora, suscipit distinctio nesciunt praesentium consequatur natus impedit expedita quo dolores nulla. Minima, nesciunt perspiciatis odio doloremque hic eligendi recusandae dignissimos vero dicta culpa tempore maxime eos non vel voluptas ut, tenetur repellat iure numquam error cum praesentium? Nam dignissimos saepe voluptate dolorem ea pariatur omnis amet unde vero aspernatur quas architecto animi laboriosam aliquam labore eum illo, tempora sit porro officiis est quos minima alias modi! Aperiam accusantium atque minus? Perferendis distinctio maiores, impedit in sit quaerat eligendi libero quidem nisi possimus voluptatem aliquam?</p>
+            </div>
+        </div>
+    </div>
+    <div class="page4">
+        <div class="header">
+            <h1>Profil</h1>
+            <p>Vocational High School</p>
+        </div>
+        <div class="content">
+            <div class="section1">
+                <div class="detail">
+                    <h2>Detail</h2>
+                    <h3>Nama : </h3>
+                    <p>Sweone Archieve</p>
+                    <h3>Jurusan : </h3>
+                    <p>XI RPL-1</p>
+                    <h3>Alamat : </h3>
+                    <p>JL.Keputih Tegal</p>
+                </div>
+                <div class="icons">
+                    <i class="fab fa-github"></i>
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-snapchat-ghost"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+            </div>
+            <div class="section2">
+                <h2>Profil Kami :</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis sunt, temporibus, corporis tenetur blanditiis, nostrum in culpa tempore nihil commodi consequatur soluta minus rem! Aspernatur, vitae. Ex deserunt magni provident nihil fugit vero non sapiente asperiores nemo eveniet hic laboriosam, facere delectus quod nesciunt esse commodi ea at quae eligendi.</p>
+            </div>
+            <div class="section3">
+                <!-- <div class="content"> -->
+                <img src="./img/WhatsApp Image 2024-02-29 at 09.05.40_fdf68d8c.jpg">
+                <div class="paragraf">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa iste deleniti dignissimos consequuntur iure architecto nulla quos. Ullam nesciunt provident totam, vitae quam dicta repellat, quidem rerum explicabo dolore itaque impedit fugit mollitia at nostrum eos dolor culpa beatae? Debitis magnam repudiandae quod corporis perferendis rem blanditiis quibusdam natus corrupti!</p>
+                </div>
+                <!-- </div> -->
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <section id="section1">
+            <div class="head">
+                <img src="./img/logo.png" width="10%">
+                <h3>RPL <br>SMKN 10 Surabaya</h3>
+            </div>
+            <div class="content">
+                <h5>Alamat : JL.Keputih Tegal, Keputih, Sukolilo, Kota SBY.Jawa Timur 60111</h5>
+                <h5>Telp : (031)5939581</h5>
+                <h5>Email : Info@smkn10surabaya.sch.id</h5>
+                <h5>Akreditasi A</h5>
+                <h3>Hak Cipta©2024 <span>XI RPL-I</span> </h3>
+            </div>
+        </section>
+        <section id="section2">
+            <img src="./img/logo.png">
+            <h3>RPL <br>SMKN 10 Surabaya</h3>
+        </section>
+    </footer>
+</body>
+
+</html>
